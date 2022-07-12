448f2a3541fccc4aff92b508a0b3756c8b181beb
Santiago Denis <santidenis.sd@gmail.com>
2022-07-12 13:44:23 -0300

    [FIX] app.css typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import './app.css';
+import './App.css';
 import Footer from './components/footer/Footer';
 import Landing from './components/landing/Landing';
 import Navbar from './components/navbar/Navbar';
