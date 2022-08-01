ab03c4e12c637de741bf7458b71d380b505f606a
Santiago Denis <santidenis.sd@gmail.com>
2022-08-01 16:04:46 -0300

    [ADD] responsive design

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,23 +12,22 @@ const Contact = () => {
                         <h1>Datos de Contacto</h1>
                         <div className="contactData_icons">
                             <div className="contactData_icon">
-                                <Icon icon="ep:location" style={{width: '8%', height:'8%', marginRight: '2.5%', marginTop: '1%'}}/>
-                                <p>Calle Sucre N 1290 (Taller Creativo), Sullana, Piura</p>
+                                <Icon icon="ep:location"/>
+                                <p>&nbsp; &nbsp; &nbsp; Calle Sucre N 1290 (Taller <br /> &nbsp; &nbsp; &nbsp; Creativo), Sullana, Piura</p>
                             </div>
                             <div className="contactData_icon">
-                                <Icon icon="mdi:gmail" style={{width: '8%', height:'8%', marginRight: '2.5%', marginTop: '1%'}}/>
-                                <p>eday@gmail.com</p>
-                            </div>
-                            <div className="contactData_icon">
-                                <Icon icon="bi:telephone-plus-fill" style={{width: '7%', height:'7%', marginRight: '3%', marginTop: '1.5%'}}/>
-                                <p>+51-939731510 (Pedidos) <br />
-                                +51-939731514 (Estudio Creativo)
+                                <Icon icon="bi:telephone-plus-fill"/>
+                                <p>&nbsp; &nbsp; &nbsp; +51-939731510 (Pedidos) <br />
+                    &nbsp; &nbsp; &nbsp; +51-939731514 (Estudio Creativo)
                                 </p>
-
                             </div>
                             <div className="contactData_icon">
-                                <Icon icon="akar-icons:whatsapp-fill" style={{width: '7%', height:'7%', marginRight: '3%', marginTop: '1.8%'}}/>
-                                <p>+51 939 731 510</p>
+                                <Icon icon="mdi:gmail"/>
+                                <p>&nbsp; &nbsp; &nbsp; eday@gmail.com</p>
+                            </div>
+                            <div className="contactData_icon">
+                                <Icon icon="akar-icons:whatsapp-fill"/>
+                                <p>&nbsp; &nbsp; &nbsp; +51 939 731 510</p>
                             </div>
                         </div>
                     </div>
@@ -40,7 +39,9 @@ const Contact = () => {
 
             <div className="location">
                 <h1>GPS Navegación</h1>
-                <div className="location__map"></div>
+                <div className="location__map">
+                    <iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=Sucre%201290,%20Sullana,%20Per%C3%BA&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
             </div>
 
             <div className="communications">
